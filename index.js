45d0d6fecab6ddd398a99206df77d38f44853255
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2022-04-24 23:49:38 +0100

    Set up query for products category

--- index.js
+++ index.js
@@ -127,6 +127,7 @@ const typeDefs = gql`
     price: Float!
     image: String!
     onSale: Boolean!
+    category: Category
   }
 
   type Category {
@@ -155,10 +156,46 @@ const resolvers = {
     }
   },
 
+
+  /*
+    query {
+      category(id: "d914aec0-25b2-4103-9ed8-225d39018d1d") {
+        name
+        products {
+          name
+          description
+          image
+        }
+      }
+    }
+  */
   Category: {
     products: (parent, args, context) => {
       const categoryId = parent.id
       return products.filter((product) => product.categoryId === categoryId)
+    }
+  },
+
+
+  /*
+    query {
+      products {
+        name
+        category {
+          name
+            products {
+              description
+              name
+              quantity
+            }
+        }
+      }
+    }
+  */
+  Product: {
+    category: (parent, args, context) => {
+      const {categoryId} = parent
+      return categories.find((category) => category.id === categoryId)
     }
   }
 }
